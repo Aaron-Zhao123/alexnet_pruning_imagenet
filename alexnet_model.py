af95a4c1b9c1b3fd175596882bd2ba1502942c4a
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-06-27 21:27:22 +0100

    wrong size

--- alexnet_model.py
+++ alexnet_model.py
@@ -42,7 +42,7 @@ class alexnet(object):
     def conv_network(self, images):
         imgs = images
 
-        conv1 = self.conv_layer(imgs, 'conv1', padding = 'SAME', stride = 4, prune = True)
+        conv1 = self.conv_layer(imgs, 'conv1', padding = 'VALID', stride = 4, prune = True)
         pool1 = self.maxpool(conv1, 'pool1', 3, 2, padding = 'VALID')
         lrn1 = self.lrn(pool1, 'lrn1')
 
