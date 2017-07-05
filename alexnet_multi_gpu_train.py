30651d50d46f35b34b06a70aa10751dd5babc3ab
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-07-05 22:05:00 +0100

    added load

--- alexnet_multi_gpu_train.py
+++ alexnet_multi_gpu_train.py
@@ -251,6 +251,7 @@ def train():
 
       # Save the model checkpoint periodically.
       if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+        print("Storing model...")
         checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
         saver.save(sess, checkpoint_path, global_step=step)
 
